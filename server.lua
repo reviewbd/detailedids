dc584ca2051c089c4aa6e647c70fb231c5e5cc67
ReviewB <103180532+reviewbd@users.noreply.github.com>
2025-03-28 21:10:43 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,4 +1,5 @@
 ESX = exports["es_extended"]:getSharedObject()
+MySQL = exports['oxmysql'] -- Utilisation de OXMySQL
 
 RegisterNetEvent("requestPlayerInfo")
 AddEventHandler("requestPlayerInfo", function()
@@ -6,12 +7,35 @@ AddEventHandler("requestPlayerInfo", function()
     local xPlayer = ESX.GetPlayerFromId(_source)
 
     if xPlayer then
-        local playerId = _source
-        local playerName = GetPlayerName(_source) -- Nom FiveM
-        local rpFirstName = xPlayer.get("firstName") or "Inconnu"
-        local rpLastName = xPlayer.get("lastName") or "Inconnu"
+        local uuid = xPlayer.get("uuid") -- Vérification correcte
 
-        -- Envoie les infos au client
-        TriggerClientEvent("displayPlayerInfo", _source, playerId, playerName, rpFirstName, rpLastName)
+        if uuid and uuid ~= "" then
+            MySQL.scalar("SELECT uuid FROM users WHERE uuid = ?", {uuid}, function(dbUuid)
+                SendPlayerInfo(_source, xPlayer, dbUuid or "Pas d'UUID")
+            end)
+        else
+            print("[detailedids] Erreur: UUID invalide pour le joueur " .. _source)
+            SendPlayerInfo(_source, xPlayer, "Pas d'UUID")
+        end
+    else
+        print("[detailedids] Erreur: Impossible de récupérer xPlayer pour " .. _source)
     end
 end)
+
+-- Fonction pour envoyer les infos au client
+function SendPlayerInfo(playerId, xPlayer, uuid)
+    local playerName = GetPlayerName(playerId) -- Nom FiveM
+    local rpFirstName = xPlayer.get("firstName") or "Inconnu"
+    local rpLastName = xPlayer.get("lastName") or "Inconnu"
+    local job = xPlayer.getJob().label or "Sans emploi" -- Récupère le job
+
+    -- Envoie les infos au client
+    TriggerClientEvent("displayPlayerInfo", playerId, {
+        id = playerId,
+        fivemName = playerName,
+        firstName = rpFirstName,
+        lastName = rpLastName,
+        uuid = uuid,
+        job = job
+    })
+end
